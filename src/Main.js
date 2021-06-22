b5e6e3586e222db7fca5bf8e18ab07fa98442130
eradaali <erada.abdalrhman@gmail.com>
2021-06-22 23:28:41 +0300

    change Style

--- src/Main.js
+++ src/Main.js
@@ -2,31 +2,20 @@
 import React, { Component } from 'react'
 import Hornedbeasts from './Hornedbeasts'
 import hornsData from './hornsData'
+import { Row} from 'react-bootstrap';
 // import { Button } from 'reactstrap';
 class Main extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         galary: 0
-    //     }
-    // }
-
-    // const printImages=()
+   
     render() {
         // console.log(typeof this.state.galary);
         // console.log(this.state)
         return (
+            <Row>{
             hornsData.map(i => {
 
                 return (
-                    <div>
-                        {/* <hornsData /> */}
-                        {/* <h1>{this.props.title}</h1>
-                <p>{this.props.description}</p>
-                <h2>{this.props.keyword}</h2>
-                <h3>{this.props.horns}</h3> */}
-                        {/* <img src={this.props.image_url} style={{width: 250}} /> */}
-                        {/* <button onClick={this.react}></button> */}
+                   
+                    
 
                         <Hornedbeasts
                             imgVarible={i.image_url}
@@ -37,9 +26,10 @@ class Main extends Component {
 
                         />
 
-                    </div >
-             ) })
-        )
+                 
+             ) }) }
+             </Row > )
+        
     }
 }
 export default Main
