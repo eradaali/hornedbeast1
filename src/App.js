9b614484d6f712b414b3cf41ab03c27d445dccd9
eradaali <erada.abdalrhman@gmail.com>
2021-06-24 21:59:35 +0300

    Done day 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import SelectHornes from './SelectHornes';
 import ModelHornedbeats from './ModelHornedbeats';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from 'react-bootstrap';
@@ -12,6 +13,7 @@ export class App extends React.Component {
       <div>
        <ModelHornedbeats/>
         <Header />
+        <SelectHornes/>
         <Container>
         <Main  />
         </Container>
